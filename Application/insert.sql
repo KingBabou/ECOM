ae2dc6d593d6d867c2f80349763673ec7f9d3393
KingBabou <tib-noug@hotmail.fr>
2016-12-13 08:49:34 +0100

    Modification du fichier d'insertion des valeurs tests dans la base de données

--- Application/insert.sql
+++ Application/insert.sql
@@ -2,7 +2,26 @@ insert into UTILISATEUR (NOM, PRENOM, PSEUDONYME, ADRESSE, MDP, ADMINISTRATEUR) 
 insert into UTILISATEUR (NOM, PRENOM, PSEUDONYME, ADRESSE, MDP, ADMINISTRATEUR) values ('Ramel', 'Régis', 'Linkdirt', 'Praxus City', 'titi', 0);
 
 insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Ordinateur à prix cassé', 500, 'Ordinateur à prix cassé pour la mondique somme de 500€', curdate(), 'Informatique');
-insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Vélo', 50, 'Vélo pour enfant de 2 à 5 ans', curdate(), 'Jouet');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Machine à café Nespresso Citiz & Milk', 144, 'Machine à café Nespresso Citiz & Milk', curdate(), 'Cuisine');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Xperia Z3 Tablet Compact 8"', 199.99, 'Xperia Z3 Tablet Compact 8"', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'LEGO - Star Wars', 85.05, 'LEGO - Star Wars', curdate(), 'Jouet');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'SAMSUNG - TV LED 40" - 4K UHD', 499.99, 'SAMSUNG - TV LED 40" - 4K UHD', curdate(), 'Autre');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Ordinateur à prix cassé', 500, 'Ordinateur à prix cassé pour la mondique somme de 500€', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Machine à café Nespresso Citiz & Milk', 144, 'Machine à café Nespresso Citiz & Milk', curdate(), 'Cuisine');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Xperia Z3 Tablet Compact 8"', 199.99, 'Xperia Z3 Tablet Compact 8"', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'LEGO - Star Wars', 85.05, 'LEGO - Star Wars', curdate(), 'Jouet');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'SAMSUNG - TV LED 40" - 4K UHD', 499.99, 'SAMSUNG - TV LED 40" - 4K UHD', curdate(), 'Autre');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Ordinateur à prix cassé', 500, 'Ordinateur à prix cassé pour la mondique somme de 500€', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Machine à café Nespresso Citiz & Milk', 144, 'Machine à café Nespresso Citiz & Milk', curdate(), 'Cuisine');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Xperia Z3 Tablet Compact 8"', 199.99, 'Xperia Z3 Tablet Compact 8"', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'LEGO - Star Wars', 85.05, 'LEGO - Star Wars', curdate(), 'Jouet');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'SAMSUNG - TV LED 40" - 4K UHD', 499.99, 'SAMSUNG - TV LED 40" - 4K UHD', curdate(), 'Autre');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Ordinateur à prix cassé', 500, 'Ordinateur à prix cassé pour la mondique somme de 500€', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'Machine à café Nespresso Citiz & Milk', 144, 'Machine à café Nespresso Citiz & Milk', curdate(), 'Cuisine');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'Xperia Z3 Tablet Compact 8"', 199.99, 'Xperia Z3 Tablet Compact 8"', curdate(), 'Informatique');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (1, 'LEGO - Star Wars', 85.05, 'LEGO - Star Wars', curdate(), 'Jouet');
+insert into ANNONCE (ID_UTILISATEUR, TITRE, PRIX, DESCRIPTION, CREATION, TYPE) values (2, 'SAMSUNG - TV LED 40" - 4K UHD', 499.99, 'SAMSUNG - TV LED 40" - 4K UHD', curdate(), 'Autre');
+
 
 insert into IMAGE(ID_ANNONCE, URL) values (1, 'http://www.cekane.fr/wp-content/uploads/2015/10/googlelogosept12015.png');
 insert into IMAGE(ID_ANNONCE, URL) values (2, 'http://www.cekane.fr/wp-content/uploads/2015/10/googlelogosept12015.png');
